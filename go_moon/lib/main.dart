89a372591d8916d48815a9f4aa4d3c3ba7aa46af
Lim Wen Hao <wenhaojshs@gmail.com>
2024-09-10 15:54:55 +0800

    Resolve warning

--- go_moon/lib/main.dart
+++ go_moon/lib/main.dart
@@ -14,7 +14,7 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: 'GoMoon',
       theme: ThemeData(
-        scaffoldBackgroundColor: Color.fromRGBO(31, 31, 31, 1.0),
+        scaffoldBackgroundColor: const Color.fromRGBO(31, 31, 31, 1.0),
       ),
       home: HomePage()
     );
