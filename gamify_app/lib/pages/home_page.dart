194b25a0005241a8589b93cc95b3b51c4f83d0fc
WenHao1223 <wenhaojshs@gmail.com>
2024-10-03 20:11:54 +0800

    Setup project & learn background gradient, alignment

---
+++ gamify_app/lib/pages/home_page.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:gamify_app/data.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  double? _deviceHeight, _deviceWidth;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          _featuredGamesWidget(),
+          _gradientBoxWidget(),
+        ],
+      ),
+    );
+  }
+
+  Widget _featuredGamesWidget() {
+    return SizedBox(
+      height: _deviceHeight! * 0.50,
+      width: _deviceWidth!,
+      child: PageView(
+        children: featuredGames.map((game) {
+          return Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage(
+                  game.coverImage.url,
+                ),
+              ),
+            ),
+          );
+        }).toList(),
+      ),
+    );
+  }
+
+  Widget _gradientBoxWidget() {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        height: _deviceHeight! * 0.80,
+        width: _deviceWidth!,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(35, 45, 59, 1.0),
+              Colors.transparent,
+            ],
+            stops: [
+              0.65,
+              1.0,
+            ],
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+          ),
+        ),
+      ),
+    );
+  }
+}
